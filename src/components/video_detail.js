4e0913b6e2ec931a53fb496b1c192f14648c4d40
megix159 <megix159@gmail.com>
2016-11-21 14:46:09 -0500

    added video details bug fixes

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -6,14 +6,16 @@ const VideoDetail = ({video}) => {
 	}
 
 	const videoId = video.id.videoId;
-	const url = `https://www.youtube.com/embed/${videoID}`;
+	const url = `https://www.youtube.com/embed/${videoId}`;
+
+
 	return(
 		<div className="video-detail col-m-8">
 			<div className="embed-responsive embed-responsive-16by9">
 				<iframe className="embed-responsive-item" src={url}></iframe>
 			</div>
 			<div className="details">
-				<div>{videos.snippet.title}</div>
+				<div>{video.snippet.title}</div>
 				<div>{video.snippet.description}</div>
 			</div>
 		</div>
